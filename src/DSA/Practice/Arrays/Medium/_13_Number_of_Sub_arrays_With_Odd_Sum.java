0c49f0d305c044a28eb5b36fd1eb872750af7c21
Anvith S G <anvithsg2004@gmail.com>
2025-03-01 20:12:06 +0530

    Tried Yesterdays LeetCode POTD

--- src/DSA/Practice/Arrays/Medium/_13_Number_of_Sub_arrays_With_Odd_Sum.java
+++ src/DSA/Practice/Arrays/Medium/_13_Number_of_Sub_arrays_With_Odd_Sum.java
@@ -1,44 +1,44 @@
-package DSA.Practice.Arrays.Medium;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-public class _13_Number_of_Sub_arrays_With_Odd_Sum {
-
-    //Brute Force Solution
-    //Got Time Complexity Error
-    public int numOfSubarrays(int[] arr) {
-
-        int n = arr.length;
-
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum = sum + arr[j];
-            }
-            if (sum % 2 == 1) {
-                count++;
-            }
-        }
-
-        return count;
-
-    }
-
-    //Better Solution
-    public static int numOfSubarrays2(int[] arr) {
-        int count = 0;
-
-        //Odd - Even = Odd ✅
-        //Even - Odd = Odd ✅
-        //Even - Even = Even ❌
-        //Odd - Odd = Even ❌
-
-
-        return count;
-    }
-}
+//package DSA.Practice.Arrays.Medium;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class _13_Number_of_Sub_arrays_With_Odd_Sum {
+//
+//    //Brute Force Solution
+//    //Got Time Complexity Error
+//    public int numOfSubarrays(int[] arr) {
+//
+//        int n = arr.length;
+//
+//        int count = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            int sum = 0;
+//            for (int j = i; j < n; j++) {
+//                sum = sum + arr[j];
+//            }
+//            if (sum % 2 == 1) {
+//                count++;
+//            }
+//        }
+//
+//        return count;
+//
+//    }
+//
+//    //Better Solution
+//    public static int numOfSubarrays2(int[] arr) {
+//        int count = 0;
+//
+//        //Odd - Even = Odd ✅
+//        //Even - Odd = Odd ✅
+//        //Even - Even = Even ❌
+//        //Odd - Odd = Even ❌
+//
+//
+//        return count;
+//    }
+//}
